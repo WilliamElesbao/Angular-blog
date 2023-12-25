3fe09898dfad3fa81989933039b7db39ab3a07a8
WilliamElesbao <william.elesbao.2000@gmail.com>
2023-12-24 22:40:03 -0300

    content created

--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -7,6 +7,7 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class SmallCardComponent implements OnInit {
 
+  @Input() id: string = '0'
   @Input() photoCover: string = ''
   @Input() cardTitle: string =  ''
 
---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,28 @@
+export const dataFake = [
+  {
+    'id': '1',
+    'photoCover': 'https://divineinfosys.com/wp-content/uploads/2022/03/1_SFxBtjAaTAC2rsbFW43K0A.png',
+    'title': 'New Programming Skill Unlocked',
+    'description': 'William has been dedicating himself and learning JavaScript, TypeScript and Angular in a self-taught way, through the DIO (Digital Innovation One) platform, thus expanding his frontend and backend programming skills.'
+  },
+  {
+    'id': '2',
+    'photoCover': 'https://techworm.net/programming/wp-content/uploads/2019/07/Microsofts-TypeScript-Breaks-Into-RedMonk%E2%80%99s-Top-10-Programming-Language-List-For-The-First-Time.jpg',
+    'title': 'Linguagem de programação baseada em JavaScript...',
+    'description': 'Uma linguagem de programação baseada em JavaScript, oferecendo tipagem estática opcional e recursos avançados para desenvolvimento de aplicativos robustos e escaláveis.'
+  },
+  {
+    'id': '3',
+    'photoCover': 'https://e0.pxfuel.com/wallpapers/210/608/desktop-wallpaper-programming-javascript-javascript-code.jpg',
+    'title': 'Linguagem de programação essencial para desenvolvimento web...',
+    'description': 'Uma linguagem de programação essencial para desenvolvimento web, permitindo interações dinâmicas e funcionais nos navegadores, amplamente usada para criar sites interativos e aplicativos web.'
+  },
+  {
+    'id': '4',
+    'photoCover': 'https://i.redd.it/g38817mqb1361.png',
+    'title': 'Uma plataforma de hospedagem e controle de versão de código-fonte...',
+    'description': ' Uma plataforma de hospedagem e controle de versão de código-fonte, permitindo colaboração, gerenciamento de projetos e controle de alterações em código, facilitando o desenvolvimento em equipe.'
+  }
+
+
+]
